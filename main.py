9277904682c396c4acc15aa1b73ae611b8a8fdbf
redscorpes <25542572-redscorpes@users.noreply.replit.com>
2023-11-22 11:27:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+start = 900
+monthly = 20
+print("what month would you like to know?")
+month = int(input())
+
+print("what year would you like to know?")
+year = int(input())
+
+total_months = (year - 2023 - 1) * 12 + 1 + month
+
+current_money = total_months * 20 + 900
+
+print(current_money)
